113736c75b56f7c47b514ecb83ad240455a32f77
Guillermo Trujillo García <guillermo@trujillogarcia.com>
2023-06-15 13:30:13 +0100

    change name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angularfiebase-authentication'),
+      dir: require('path').join(__dirname, './coverage/spiralverse-front'),
       subdir: '.',
       reporters: [
         { type: 'html' },
