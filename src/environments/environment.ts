32e6fc91a96049f2044274b38dae0155a2929a75
Guillermo Trujillo García <guillermo@trujillogarcia.com>
2023-10-27 13:33:00 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDk1Emvils8Zz8_nOx3u514ax9Wdr23Kgw",
-    authDomain: "spiralverse.firebaseapp.com",
-    projectId: "spiralverse",
-    storageBucket: "spiralverse.appspot.com",
-    messagingSenderId: "430568456629",
-    appId: "1:430568456629:web:d7481a766b8137dace59fc",
-    measurementId: "G-NYY3T2PKYK"
+    apiKey: '${{ secrets.FIREBASE_API_KEY_SPIRALVERSE }}',
+    authDomain: 'spiralverse.firebaseapp.com',
+    projectId: 'spiralverse',
+    storageBucket: 'spiralverse.appspot.com',
+    messagingSenderId: '${{ secrets.FIREBASE_MESSAGING_SENER_ID_SPIRALVERSE }}',
+    appId: '${{ secrets.FIREBASE_APP_ID_SPIRALVERSE }}',
+    measurementId: '${{ secrets.FIREBASE_MEASUREMENT_ID_SPIRALVERSE }}'
   }
 };
